7f9465bbbee49b6b1f394a79564036ce2ca1fd74
BuildTools <pq164536@uni-greifswald.de>
2021-11-19 18:04:04 +0100

    hold to move (rough)

--- angleKruemel/main.py
+++ angleKruemel/main.py
@@ -7,13 +7,15 @@ h, w = 540, 960
 
 screen = pygame.display.set_mode([w, h])
 
+ButtonPresses = [False, False, False, False]
+pressed = False
 
 #game variable
 running = True
 
 FPS = 60
-x = 250
-y = 250
+source = [200,200]
+direction = [0, 0, 0, 0]
 
 steps = 10
 
@@ -21,7 +23,12 @@ clock = pygame.time.Clock()
 
 while running:
 
-    player = pygame.Rect(x, y, 20, 20)
+    source[0] = source[0] + direction[0] - direction[1]
+    source[1] = source[1] + direction[2] - direction[3]
+
+    direction = [0, 0, 0, 0]
+
+    player = pygame.Rect(source[0], source[1], 20, 20)
 
     events = pygame.event.get()
     for event in events:
@@ -29,14 +36,32 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                y += 1 * steps
+                source[0] += 1 * steps
+                ButtonPresses[0] = True
             if event.key == pygame.K_UP:
-                y -= 1 * steps
+                source[0] -= 1 * steps
+                ButtonPresses[1] = True
             if event.key == pygame.K_LEFT:
-                x -= 1 * steps
+                source[1] -= 1 * steps
+                ButtonPresses[2] = True
             if event.key == pygame.K_RIGHT:
-                x += 1 * steps
-                
+                source[1] += 1 * steps
+                ButtonPresses[3] = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                ButtonPresses[0] = False
+            if event.key == pygame.K_UP:
+                ButtonPresses[1] = False
+            if event.key == pygame.K_LEFT:
+                ButtonPresses[2] = False
+            if event.key == pygame.K_RIGHT:
+                ButtonPresses[3] = False
+    
+    for i in range(0,4,1):
+        if ButtonPresses[i]:
+            direction[i] += 1 * steps
+
+    
     screen.fill((255, 0, 0))
     #screen update
     pygame.draw.rect(screen, (255, 255, 255), player)
