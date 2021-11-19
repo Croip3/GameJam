1b66c7848ffcc2c027c17cc45955b31b44c9e5c6
Christopher <37404705+Croip3@users.noreply.github.com>
2021-11-19 16:27:53 +0100

    added pygame

---
+++ angleKruemel/main.py
@@ -0,0 +1,38 @@
+print ("angleKruemel gestartet")
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode([400, 400])
+
+running = True
+
+x = 250
+y = 250
+
+steps = 10
+
+while running:
+
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                y += 1 * steps
+            if event.key == pygame.K_UP:
+                y -= 1 * steps
+            if event.key == pygame.K_LEFT:
+                x -= 1 * steps
+            if event.key == pygame.K_RIGHT:
+                x += 1 * steps
+                
+    screen.fill((255, 0, 0))
+
+    pygame.draw.rect(screen, (255, 255, 255), (x, y, 20, 20))    
+
+    #screen update
+    pygame.display.flip()
+
+pygame.quit()
