3e3a71785e189c7b4c7ce63d3b0a8ee5af249711
BuildTools <pq164536@uni-greifswald.de>
2021-11-20 12:56:37 +0100

    collision fertig und punktesystem

--- angleKruemel/colliding.py
+++ angleKruemel/colliding.py
@@ -1,5 +1,38 @@
 import pygame
-import numpy as np
+
+oldsource = [-1,-1]
+collideArray = []
+
+def applyHardCollision(player, listOfObstacles, source):
+
+    global oldsource
+    global collideArray
+
+    closedKeys = set()
+    collideArray = []
+
+    if oldsource == [-1,-1]:
+        oldsource = source
+
+    for obstacle in listOfObstacles:
+        if pygame.Rect.colliderect(player,obstacle):
+            #get infos about the direction, which the player is colliding
+            collideInfo = directionCollided(pygame.Rect(oldsource[0],oldsource[1],20,20), player, obstacle)
+            collideArray.append(collideInfo)
+            #preserve gravitation
+            for collideInfo in collideArray:
+                closedKeys.add(collideInfo[2])
+            
+            #collision resets you to your old point
+
+            if "left" in closedKeys or "right" in closedKeys:
+                source[0] = oldsource[0]
+            if "up" in closedKeys or "down" in closedKeys:
+                source[1] = oldsource[1]
+
+    oldsource = source.copy()
+
+    return [closedKeys,source]
 
 #this method is there to return the directions in which the player is colliding with something
 def directionCollided(old, new, obstacle):
@@ -32,7 +65,7 @@ def directionCollided(old, new, obstacle):
         CollideDir[2] = "left"
     #same thing, just from the right
     elif old.x > obstacle.x + obstacle.width:
-        CollideDir[2] = "Right"
+        CollideDir[2] = "right"
     #if both dont apply, than you are under or over the obstackle. Therefore we need a simple distinction of cases
     else:
         if CollideDir[1] < 0:
--- angleKruemel/main.py
+++ angleKruemel/main.py
@@ -12,12 +12,18 @@ screen = pygame.display.set_mode([w, h])
 #movement variables
 steps = 30
 source = [200,200]
-oldsource = [200,200]
 player = pygame.Rect(source[0], source[1], 20, 20)
 listOfObstacles = [pygame.Rect(0,400,800,20), pygame.Rect(200,300,800,20),pygame.Rect(400,380,20,20)]
+listOfBlessings = [pygame.Rect(300,370,20,20)]
+listOfCookies = []
 
 #game variables
 running = True
+lifes = 0
+
+#text variables
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
 #framerate variables
 FPS = 60
@@ -40,27 +46,29 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #find out which obstacle the player is colliding with
-    for obstacle in listOfObstacles:
-        if pygame.Rect.colliderect(player,obstacle):
-            #get infos about the direction, which the player is colliding
-            collideInfo = cl.directionCollided(pygame.Rect(oldsource[0],oldsource[1],20,20), player, obstacle)
-            collideArray.append(collideInfo)
-            #preserve gravitation
-            if collideInfo[2] == "up":
-                isFalling = False
-            #collision resets you to your old point
-            source = oldsource.copy()
-    
+    #applying HardCollision for strong objects
+    result = cl.applyHardCollision(player,listOfObstacles,source)
+    closedKeys = result[0].copy()
+    source = result[1].copy()
+
+    if "up" in closedKeys:
+        isFalling = False
+
+    for collectible in listOfBlessings:
+        if pygame.Rect.colliderect(player,collectible):
+            listOfBlessings.remove(collectible)
+            lifes += 1
+
     #draw the game
     player.update(source[0], source[1], 20, 20)
     screen.fill((255, 0, 0))
+    textsurface = myfont.render('Punkte: '+str(lifes), False, (0, 0, 0))
+    screen.blit(textsurface, (0,0))
     for obstacle in listOfObstacles:
         pygame.draw.rect(screen, (255, 255, 0), obstacle)
+    for collectibles in listOfBlessings:
+        pygame.draw.rect(screen, (255, 255, 0), collectibles)
     pygame.draw.rect(screen, (255, 255, 255), player)
-
-    #save the previus position
-    oldsource = source.copy()
 
     #calculating destiny of player and updating it
     source = mv.movementHandler(steps, source, delta, events, collideArray)
