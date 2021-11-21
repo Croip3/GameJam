2c0fbc47cc9847d22f226562b3c020408ab4f424
BuildTools <pq164536@uni-greifswald.de>
2021-11-21 14:43:33 +0100

    rdy game

--- angleKruemel/vogel.py
+++ angleKruemel/vogel.py
@@ -1,10 +1,12 @@
 import pygame
 
-class vogel(pygame.sprite.Sprite):
+class Vogel(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y, scale):
         super().__init__()
         self.x = pos_x
         self.y = pos_y
+
+        
 
         self.image = pygame.image.load("angleKruemel\sprites\hover\BossBieneAnimationHover_0006_Frame-1.png")
 
@@ -13,3 +15,6 @@ class vogel(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         self.rect.topleft = [pos_x, pos_y]
+
+    def updateRect(self,x,y):
+        self.rect.topleft = [x, y]
